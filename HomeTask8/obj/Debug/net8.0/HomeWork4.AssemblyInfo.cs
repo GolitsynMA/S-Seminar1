78d971046b536ffc9da2b431afe437951b643c05
«Misha» <golitsynma@example.com>
2023-08-03 22:39:49 +0300

    add home task 8

--- HomeTask8/obj/Debug/net8.0/HomeWork4.AssemblyInfo.cs
+++ HomeTask8/obj/Debug/net8.0/HomeWork4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95e3552e29d56818cb175e09a80640b90d63bb6")]
-[assembly: System.Reflection.AssemblyProductAttribute("Task1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
+[assembly: System.Reflection.AssemblyProductAttribute("HomeWork4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("HomeWork4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
