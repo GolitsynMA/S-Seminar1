e95e3552e29d56818cb175e09a80640b90d63bb6
«Misha» <golitsynma@example.com>
2023-08-03 21:20:36 +0300

    add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+int result = number * number;
+Console.WriteLine("Наш результат: " + result);
